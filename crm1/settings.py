a7e56798fe8370589b985ee132075866adbb95d2
bishal01 <bishaladhikari2056@gmail.com>
2022-11-10 21:44:18 +0545

    second

--- crm1/settings.py
+++ crm1/settings.py
@@ -24,8 +24,12 @@ SECRET_KEY = 'cz=&%f*9(d*zo$_55p=(p)(eki#p$pb^0159-)8k^6$9c3l&_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = ['web-production-d656.up.railway.app','127.0.0:1']
+
+CSRF_TRUSTED_ORIGINS = ['web-production-d656.up.railway.app','127.0.0:1']
+
+
+
 
 
 # Application definition
